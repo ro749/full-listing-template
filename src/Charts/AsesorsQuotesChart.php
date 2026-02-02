91bcc9514457aaf100d4dc21a4b2e5fb9a86ebf2
ro749 <45647699+ro749@users.noreply.github.com>
2026-02-02 11:04:02 -0600

    Update AsesorsQuotesChart.php

--- src/Charts/AsesorsQuotesChart.php
+++ src/Charts/AsesorsQuotesChart.php
@@ -3,7 +3,7 @@
 namespace Ro749\FullListingTemplate\Charts;
 
 use Ro749\SharedUtils\Charts\BaseChart;
-use Ro749\SharedUtils\Getters\BaseGetter;
+use Ro749\SharedUtils\Getters\CategoryGetter;
 use Ro749\SharedUtils\Models\LogicModifiers\ForeignKey;
 use Ro749\SharedUtils\Statistics\Statistic;
 use Ro749\FullListingTemplate\Models\Quotation;
@@ -12,15 +12,17 @@ use Ro749\SharedUtils\Statistics\StatisticColumn;
 use Ro749\SharedUtils\Statistics\StatisticType;
 use Ro749\SharedUtils\Statistics\StatisticLink;
 use Ro749\SharedUtils\Tables\Column;
+use Ro749\FullListingTemplate\Enums\Options as OptionsEnum;
 
 class AsesorsQuotesChart extends BaseChart
 {
     public function __construct()
     {
         parent::__construct(
-            data_column: 'asesors',
-            label_column: 'label_date',
-            getter: new BaseGetter(
+            data_column: 'quote_count',
+            label_column: 'label',
+            getter: new CategoryGetter(
+                option_name: OptionsEnum::AsesorCategories->value,
                 columns: [
                     'quote_count'=> new Column(
                         display:"Cotizaciones",
@@ -31,7 +33,7 @@ class AsesorsQuotesChart extends BaseChart
                 ],
                 statistics:
                 [
-                    'quotes'=>new Statistic(
+                    'quote_stats'=>new Statistic(
                         model_class: Quotation::get_class(),
                         group_column: 'status',
                         columns: [
