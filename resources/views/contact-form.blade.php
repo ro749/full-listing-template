1477b386aca42fc5a72c75483fe6a64a483812b4
ro749 <45647699+ro749@users.noreply.github.com>
2026-03-31 13:22:41 -0600

    Reapply "changes"
    This reverts commit a5d1598862dd58ec480b59dc8e2df10587f5185e.

---
+++ resources/views/contact-form.blade.php
@@ -0,0 +1,13 @@
+<x-smartForm :form="$form" style="margin: 1rem; margin-top:3rem; display: flex; flex-direction: column; gap: 1rem; align-items: center;">
+    <div><p style="font-size: 3rem;"><b>Contáctanos</b></p></div>
+    <x-field name="name" :form="$form"/>
+    <div style="display:flex; flex-direction:row; gap: 1rem; width: 100%;">
+        <x-field name="email" :form="$form"/>
+        <x-field name="phone" :form="$form"/>
+    </div>
+    <x-field name="unit" :form="$form"/>
+    <x-field name="message" :form="$form"/>
+    <button class="btn btn-light" @click="submit">
+        {{ $form->submit_text }}
+    </button>
+</x-smartForm>
